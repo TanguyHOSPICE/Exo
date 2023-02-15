5f2533a3c5641b3003ff2dede0ae7bf47d78b70b
Ht <htcvhs971@gmail.com>
2023-02-15 15:32:21 +0100

    Add checkbox  & select listener

--- scriptF2.js
+++ scriptF2.js
@@ -4,3 +4,26 @@ Dans le cas où l’utilisateur ne souhaite pas recevoir de newsletter, il pourr
 
 Affichez un message dans le span #message indiquant le pays sélectionné lorsque le choix du pays sera modifié.
 */
+let checkbox = document.querySelector('input[type="checkbox"]');
+let radio = document.querySelectorAll('input[name="btn-radio"]');
+let select = document.getElementById('pays');
+let span = document.getElementById('message');
+
+checkbox.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		alert('Vous acceptez de recevoir la newsletter');
+		for (item of radio) {
+			if (item.value == 'oui') {
+				item.checked = true;
+			}
+		}
+	} else {
+		for (item of radio) {
+			item.checked = item.value === 'non';
+		}
+	}
+});
+
+select.addEventListener('change', (e) => {
+	span.innerText += `Vous avez indiqué être en ${e.target.value}`;
+});
