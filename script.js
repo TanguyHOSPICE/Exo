f108ec333fb996ff1c14488728b99c2a766e649a
Ht <htcvhs971@gmail.com>
2023-02-15 12:51:37 +0100

    Exo Form #1

---
+++ script.js
@@ -0,0 +1,16 @@
+/** Dans un premier temps, vous créerez une classe commune à tous les éléments input.
+-Par la suite, au focus, vous devrez faire en sorte que le background de l’élément input prenne la couleur rose et que sa police prenne la couleur bleu.
+-Enfin, vous créerez un élément blur qui annulera les effets du focus. */
+let allInputs = document.getElementsByClassName('inputPinkOnFocus');
+
+Array.from(allInputs).forEach((input) => {
+	input.addEventListener('focus', (e) => {
+		e.target.style.background = 'pink';
+		e.target.style.color = 'blue';
+	});
+
+	input.addEventListener('blur', (e) => {
+		e.target.style.background = '';
+		e.target.style.color = '';
+	});
+});
