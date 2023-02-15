d1641128474885623787fe247cb9138d95ba974e
Ht <htcvhs971@gmail.com>
2023-02-15 15:06:36 +0100

    Add Exo Form 2 : indexF2, scriptF2 & style.css

---
+++ scriptF2.js
@@ -0,0 +1,6 @@
+/**Une fois la checkbox cochée, une fenêtre pop-up indiquant "Vous acceptez de recevoir la newsletter" apparaît :
+
+Dans le cas où l’utilisateur ne souhaite pas recevoir de newsletter, il pourra sélectionner le bouton radio "non".
+
+Affichez un message dans le span #message indiquant le pays sélectionné lorsque le choix du pays sera modifié.
+*/
