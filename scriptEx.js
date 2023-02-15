cf13709e981406b1c852c84cd6bf50eafc5bc791
Ht <htcvhs971@gmail.com>
2023-02-15 14:44:18 +0100

    Add listener on checkbox

--- scriptEx.js
+++ scriptEx.js
@@ -1,9 +1,16 @@
 //Déclarations
 let entertainment = document.querySelector('select');
-let animalRadioBtn = document.querySelectorAll('input[type="radio"]');
+let animalRadioButtons = document.querySelectorAll('input[type="radio"]');
 let tos = document.querySelectorAll('input[type="checkbox"]');
 
 //Affiche la sélection choisi
 entertainment.addEventListener('change', (e) => {
 	console.log(`${e.target.name}: ${e.target.value}`);
+});
+
+//Pr récup la valeur d'1 btn radio il va falloir mettre 1 listener s/ chq btn radio
+animalRadioButtons.forEach((animalRadioButton) => {
+	animalRadioButton.addEventListener('change', (e) => {
+		console.log(`${e.target.id}: ${e.target.checked}`);
+	});
 });
